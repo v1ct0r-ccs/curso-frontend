b2414cd2d761b840286a0f65f9106b059c523193
Victor Bruno <v1ct0r.ccs29@gmail.com>
2022-07-16 12:48:44 +0200

    exercicio-19

--- jquery/js/custom.js
+++ jquery/js/custom.js
@@ -63,4 +63,122 @@ $(document).ready(function(){
         alert('Produto esgotado')
     })
 
+    /*
+     * Callback
+     * entendendo ações que começam ao termino de outra
+     */
+
+    // $('.featured-item:nth(1)').hide(2000, function(){
+    //     // esse é o callback
+    //     alert( $(this).find('h4').text() + 'esgotado')
+    // })
+
+    $('.featured-item:nth(1)')
+       .hide(2000, function(){
+        // esse é o callback
+            console.log( $(this).find('h4').text() + 'esgotado')
+    })
+       .show(2000, function(){
+            console.log( $(this).find('h4').text() + 'em estoque')
+       })
+
+    /*
+    * Animações
+    */
+       const duracao = 1000 // equivalente a 1 segundo
+    $('.featured-item:nth(0)')
+       .hide(duracao)
+       .show(duracao)
+       .fadeOut(duracao)
+       .fadeIn(duracao)
+       .toggle(duracao)
+       .toggle(duracao)
+
+    $('#form-submit').on('click', function(){
+
+        e.preventDefault()
+
+        if($('#email').val() != '' ){
+
+            $('#email').animate({
+                opacity: "toggle",
+                top: "-50"
+
+            }, duracao, function(){
+                console.log($(this).val())
+            })
+        }
+            
+    });
+
+    /*
+     * Ouvinte de eventos .nav-modal-open
+     */
+
+    $('.nav-modal-open').on('click', function(e){
+
+        e.preventDefault();
+
+        let elem = $(this).attr('rel')
+
+        $('.modal-body').html($('#'+elem).html())
+        $('.modal-header h5.modal-title').html($(this).text())
+
+        let myModal = new bootstrap.Modal($('#modalId'))
+
+        myModal.show()
+
+    })
+
+    $('body').on('submit', '.modal-body .form', function(e){
+
+        e.preventDefault()
+  
+        const inputName = $('#nome')
+        const inputEmail = $('#email')
+  
+        validate(inputName)
+        validate(inputEmail)
+  
+        if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid')){
+           console.log('preencher corretamente os campos obrigatórios')
+           return false
+        } else {
+           $(this).submit()  
+        }
+     })
+  
+     $('body').on('blur', '#nome', function(){
+        validate($(this))
+     })
+  
+     $('body').on('blur', '#email', function(){
+        validate($(this))
+     })
+
+     $('body').on('blur', '#date', function(){
+        validate($(this))
+        $(this).mask('00/00/0000');
+     }) 
+  
+     $('body').on('blur', '#time', function(){
+        validate($(this))
+        $(this).mask('00:00');
+     })
+  
+     $('body').on('blur', '#cep', function(){
+        validate($(this))
+        $(this).mask('00000-000');
+     })
+  
+     $('body').on('blur', '#telefone', function(){
+        validate($(this))
+        $(this).mask('00000-0000');
+     })
+  
+     $('body').on('blur', '#cpf', function(){
+        validate($(this))
+        $(this).mask('000.000.000-00');
+     })
+
 })    
